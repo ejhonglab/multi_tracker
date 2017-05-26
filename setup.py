65dde3e7a9c24691451d9436b08042504f99c663
Tom O'Connell <toconnel@caltech.edu>
2017-05-26 16:53:48 -0700

    Replaced distutils setup with setuptools, including at least h5py and progressbar dependencies. May have missed some. May also not work.

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
 name='MultiTrackerAnalysis',
 version='0.0.1',
+install_requires=['hdf5', 'progressbar'],
 author='Floris van Breugel',
 author_email='floris@caltech.edu',
 packages = ['multi_tracker_analysis'],
