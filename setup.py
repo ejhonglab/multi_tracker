b5fb32d3e7f6adec8c8526dcb04c5fc4d8e74278
Tom O'Connell <toconnel@caltech.edu>
2017-09-17 13:53:28 -0700

    Renamed ...gui_v2 to trajectory_editor. Tried getting pyqtgraph install automatic, but seems impossible in Kinetic+, b/c only qt5 supported but Ubuntu pyqtgraph depends qt4.

--- setup.py
+++ setup.py
@@ -1,14 +1,20 @@
-from setuptools import setup
+# not using this for now, because catkin not currently equipped to prevent egg folder
+# from being created in this directory. TODO fix? necessary after "build"?
+#from setuptools import setup
+from distutils.core import setup
 
 setup(
-name='MultiTrackerAnalysis',
-version='0.0.1',
-install_requires=['h5py', 'progressbar', 'pandas', 'scipy', \
-    'sympy', 'pyqtgraph'],
-author='Floris van Breugel',
-author_email='floris@caltech.edu',
-packages = ['multi_tracker_analysis'],
-license='BSD',
-description='Analysis scripts for data collected with multi tracker',
-long_description=open('README.md').read(),
+    name='MultiTrackerAnalysis',
+    version='0.0.1',
+    author='Floris van Breugel',
+    author_email='floris@caltech.edu',
+    scripts=['multi_tracker_analysis/trajectory_editor'],
+    packages=['multi_tracker_analysis'],
+    license='BSD',
+    description='Analysis scripts for data collected with multi tracker',
+    long_description=open('README.md').read(),
 )
+
+# only supported by setuptools
+#install_requires=['h5py', 'progressbar', 'pandas', 'scipy', \
+#    'sympy', 'pyqtgraph'],
