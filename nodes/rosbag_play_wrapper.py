97873848cfb0f72b07a29e94785826045b867724
em-ye <emily.ye@berkeley.edu>
2020-02-12 20:12:23 -0800

    Fixed initial implementation of pause_after from last commit.

--- nodes/rosbag_play_wrapper.py
+++ nodes/rosbag_play_wrapper.py
@@ -7,12 +7,52 @@ import sys
 import glob
 import subprocess
 import time
+import termios
+import atexit
 
 """
 Plays any bag file in the current directory, directory passed, and also existing
 behavior of passed filename (absolute or relative).
 """
 # TODO test relative paths work for directories and files
+
+
+old_settings = None
+def start_nonblocking_stdin():
+    global old_settings
+    old_settings = termios.tcgetattr(sys.stdin)
+    new_settings = termios.tcgetattr(sys.stdin)
+    new_settings[3] = new_settings[3] & ~(termios.ECHO | termios.ICANON)
+    new_settings[6][termios.VMIN] = 0
+    new_settings[6][termios.VTIME] = 0
+    termios.tcsetattr(sys.stdin, termios.TCSADRAIN, new_settings)
+
+
+@atexit.register
+def end_nonblocking_stdin():
+    global old_settings
+    if old_settings:
+        termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_settings)
+
+
+start_nonblocking_stdin()
+def read_stdin_nonblocking():
+    # https://stackoverflow.com/questions/21791621
+
+    # Also tried these solutions, without success:
+    # From Michael Nagy's answer at https://stackoverflow.com/questions/21791621
+    # https://stackoverflow.com/questions/510357
+
+    in_str = ''
+    while True:
+        ch = os.read(sys.stdin.fileno(), 1)
+        if len(ch) == 0:
+            break
+        else:
+            in_str += ch
+
+    return in_str
+
 
 class RosbagPlayWrapper:
     def __init__(self, args):
@@ -45,7 +85,6 @@ class RosbagPlayWrapper:
             '~topic_out', 'multi_tracker/delta_video'
         )
 
-        # TODO maybe err if this is set as well as pause:=True
         # Anything less than 0 and the bag file will play without consideration
         # of this parameter. Greater than 0, and the rosbag play process will
         # get a signal to pause playback as soon as a time greater or equal to
@@ -93,16 +132,20 @@ class RosbagPlayWrapper:
         ]
         cmdline.extend(self.passthrough_args)
 
-        self.rosbag_process = subprocess.Popen(cmdline,
-            preexec_fn=subprocess.os.setpgrp, stdin=subprocess.PIPE
-        )
-
+        t0 = rospy.get_time()
         if self.pause_after < 0:
             sleep_interval_s = 5
+            sp_stdin = None
         else:
             # TODO maybe sleep less frequently when pause_after >= 0 (get dt
             # form bag? just set low dt?) always use a lower dt?
             sleep_interval_s = 0.1
+            # Since otherwise we can not write to stdin of the subprocess.
+            sp_stdin = subprocess.PIPE
+
+        self.rosbag_process = subprocess.Popen(cmdline,
+            preexec_fn=subprocess.os.setpgrp, stdin=sp_stdin
+        )
 
         # TODO just spin until Popen finishes? maybe make a blocking call?
         while not rospy.is_shutdown():
@@ -113,20 +156,42 @@ class RosbagPlayWrapper:
                 )
                 sys.exit()
             else:
-                # TODO delete
-                rospy.loginfo('ROSPY.GET_TIME() {:.2f}'.format(rospy.get_time()))
-                #
-                if (self.pause_after >= 0 and
-                    rospy.get_time() >= self.pause_after):
+                # Since this call can initially return 0.0 in rosbag play.
+                if t0 == 0.0:
+                    t0 = rospy.get_time()
 
-                    # TODO delete
-                    rospy.loginfo('TRYING TO PAUSE ROSBAG PLAY')
-                    #
+                if (self.pause_after >= 0 and t0 > 0 and
+                    rospy.get_time() - t0 >= self.pause_after):
+
+                    # Hoping to clear any existing input with this, before
+                    # sending any future input through to subprocess.
+                    _ = read_stdin_nonblocking()
 
                     # Normally, pressing spacebar pauses / plays rosbag play.
                     self.rosbag_process.stdin.write(' ')
+
+                    # It seems there were some reasons to prefer this over just
+                    # write, but this blocks and I don't want that.
+                    #self.rosbag_process.communicate(input=' '.encode())
+
                     self.pause_after = -1
-                    sleep_interval_s = 5
+
+                # Would use this if we wanted to avoid passing commands through
+                # until after we have let the automatic pausing above happen.
+                # However, that means we can not start the bagfile paused, which
+                # would mean we couldn't manually start playback only after
+                # the analysis launch file is loaded. So absent some other
+                # workaround there, I'm just always going to foward this
+                # through.
+                #if self.pause_after < 0 and sp_stdin is not None:
+                if sp_stdin is not None:
+                    # Pass stdin through to subprocess.
+                    # (since it seems we can't get behavior as if stdin were not
+                    # specified to subprocess.PIPE after initializing it that
+                    # way)
+                    in_str = read_stdin_nonblocking()
+                    if len(in_str) > 0:
+                        self.rosbag_process.stdin.write(in_str)
 
                 time.sleep(sleep_interval_s)
 
